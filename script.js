8892a3407381ad806b1e3723120b2cf05ec410bf
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-02-23 18:08:41 +1100

    removed npm packages

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-var btn = document.querySelector('button')
-
+var btn = document.querySelector("button");
 
 // Function is sitting there until it gets called
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-	css.textContent = body.style.background + ";";
+  css.textContent = body.style.background + ";";
 }
-
 
 // Generating random color
 function setRandom() {
@@ -25,22 +20,21 @@ function setRandom() {
   return hexColor;
 }
 
-
 // Setting background with random color after click
-function setRandomBackground(){
-	color1.value = setRandom();
-	color2.value = setRandom();
+function setRandomBackground() {
+  color1.value = setRandom();
+  color2.value = setRandom();
 
-	setGradient();
+  setGradient();
 }
 
 // Set Initial Background from default values
-// Function gets called to retreive default color values 
+// Function gets called to retreive default color values
 // which have been set in html input tags
-setGradient(); 
+setGradient();
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
 
-btn.addEventListener('click', setRandomBackground);
+btn.addEventListener("click", setRandomBackground);
